d2ad2bfeee4029b47ad33c533a3b1b5c3f1c8a82
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2020-04-22 11:10:55 +0200

    Parquet generation script outputting 1 encoding for strings, adding reference file softlink.
    The deltalen encoded strings are not supported by the cpp library so it needs the original parquetfile (before re-encoding by prelim).
    However, for prims this is not necessary. A softlink is nice, to help keep the scripts clean.

--- profiling/parquet-mr-custom/prelim/generate_parquetfiles.sh
+++ profiling/parquet-mr-custom/prelim/generate_parquetfiles.sh
@@ -5,8 +5,8 @@
 # Encodings plain & delta for ints, deltalen for str
 # Varying page sizes at 1 GB total data size
 
-outdir=/data/parquetfiles
 fulldatasize=$((10**9)) #1 GBytes of data
+outdir=/data/parquetfiles/new
 parquetwriter=~/workspaces/openCAPI/fast-p2a/software/cpp/test/parquetwriter_test
 
 rm *.prq
@@ -28,13 +28,17 @@ for datatype in int32 int64 str; do
 		echo "Generating $datatype files of size $fulldatasize bytes and pagesize $size_bytes bytes ($size_entries entries)"
 		echo "./run.sh test_${datatype}.prq test_${datatype}_ps${size_bytes}_plain.prq $size_entries plain"
 		echo "./run.sh test_${datatype}.prq test_${datatype}_ps${size_bytes}_delta.prq $size_entries delta"
-		./run.sh test_${datatype}.prq test_${datatype}_ps${size_bytes}_plain.prq $size_entries plain &
-		./run.sh test_${datatype}.prq test_${datatype}_ps${size_bytes}_delta.prq $size_entries delta
+		./run.sh test_${datatype}.prq test_${datatype}_ps${size_bytes}_plain.prq $size_entries plain
 		mv test_${datatype}_ps${size_bytes}_plain.prq $outdir
-		mv test_${datatype}_ps${size_bytes}_delta.prq $outdir
+		if [ $datatype != "str" ]; then
+			./run.sh test_${datatype}.prq test_${datatype}_ps${size_bytes}_delta.prq $size_entries delta
+			mv test_${datatype}_ps${size_bytes}_delta.prq $outdir
+		fi
 	done
 	if [ "$datatype" == "str" ]; then
 		mv test_str.prq $outdir #needed for str, because the cpp lib only supports plain encoding
+	else
+		ln -sf test_${datatype}_ps1000_plain.prq $outdir/test_${datatype}.prq
 	fi
 done
 
